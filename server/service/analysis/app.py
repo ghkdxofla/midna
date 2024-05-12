93c42a0dd13923d5525dacd18a9e9545632f493e
ghkdxofla <ghkdxofla@gmail.com>
2024-05-12 14:50:01 +0900

    feat: add init structures for analysis service

---
+++ server/service/analysis/app.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from service.analysis.request import DataStoreRequest
+from service.analysis.response import EmbedContentResponse
+
+analysis_router = APIRouter(prefix="/api/analysis")
+
+
+@analysis_router.post("/store")
+def store(request_body: DataStoreRequest):
+    return EmbedContentResponse(
+        message="Data stored successfully",
+        data=request_body.data,
+        status="success",
+    )
