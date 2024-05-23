6e5c0e9aedd5d9e571663f63a853a988eca13b1e
ghkdxofla <ghkdxofla@gmail.com>
2024-05-23 09:40:06 +0900

    feat: add model to save gene data

---
+++ server/model/gene.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Text, TIMESTAMP, text
+from server.database.base import Base
+
+
+class Gene(Base):
+    __tablename__ = "gene"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String)
+    data = Column(Text)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("Now()")
+    )
