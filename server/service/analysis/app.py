fba562a13c49199d8aea4e4b5a35b767b4853461
ghkdxofla <ghkdxofla@gmail.com>
2024-05-24 10:32:33 +0900

    feat: add repository to logic

--- server/service/analysis/app.py
+++ server/service/analysis/app.py
@@ -2,12 +2,19 @@ from fastapi import APIRouter
 
 from service.analysis.request import DataStoreRequest
 from service.analysis.response import EmbedContentResponse
+from repository.gene_repository import GeneRepository
 
 analysis_router = APIRouter(prefix="/api/analysis")
 
 
 @analysis_router.post("/store")
 def store(request_body: DataStoreRequest):
+    repository = GeneRepository()
+    repository.create(
+        id=request_body.id,
+        user_id=request_body.user_id,
+        data=request_body.data,
+    )
     
     return EmbedContentResponse(
         message="Data stored successfully",
--- server/service/analysis/request/data_store_request.py
+++ server/service/analysis/request/data_store_request.py
@@ -2,4 +2,5 @@ from pydantic import BaseModel
 
 
 class DataStoreRequest(BaseModel):
+    user_id: str
     data: dict
