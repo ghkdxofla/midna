834bf5cc0c990120fefccc1a4ba2fa02e10ee086
ghkdxofla <ghkdxofla@gmail.com>
2024-05-24 10:32:12 +0900

    feat: add auto increment id

--- server/model/gene.py
+++ server/model/gene.py
@@ -5,7 +5,7 @@ from server.database.base import Base
 class Gene(Base):
     __tablename__ = "gene"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, , autoincrement=True)
     user_id = Column(String)
     data = Column(Text)
     created_at = Column(
