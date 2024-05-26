aa61c29b8854a7c5ec7f5f88cea3118a4fbc1d93
ghkdxofla <ghkdxofla@gmail.com>
2024-05-26 23:40:07 +0900

    feat: add user table

--- server/model/user.py
+++ server/model/user.py
@@ -1,13 +1,11 @@
-from sqlalchemy import Column, Integer, ForeignKey, Text, TIMESTAMP, text
+from sqlalchemy import Column, Integer, TIMESTAMP, text
 from server.database.base import Base
 
 
-class Gene(Base):
-    __tablename__ = "gene"
+class User(Base):
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("user.id"))
-    data = Column(Text)
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("Now()")
     )
